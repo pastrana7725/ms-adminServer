10aae1742f69bc6f591cdb99c9645c8588be691a
pastrana7725 <ibarra7725@gmail.com>
2023-01-26 14:49:54 -0500

    26-01-2023

---
+++ src/test/java/co/edu/usbcali/adminserver/AdminServerApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.usbcali.adminserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
